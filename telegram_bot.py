39f079d32d332463b8fdaf984a3f8da1e6b75038
retro935 <edwinretro52@gmail.com>
2025-11-12 00:59:15 -0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,7 +19,7 @@ PROMPT_FILE = "system-prompt.txt"
 USER_LANG_FILE = "user_langs.json"
 
 MODEL_CONFIG = {
-    "name": "agentica-org/deepcoder-14b-preview:free",
+    "name": "deepseek/deepseek-r1:free",
     "base_url": "https://openrouter.ai/api/v1",
     "key": os.getenv("OPENROUTER_KEY"),
 }
