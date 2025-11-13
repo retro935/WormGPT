42c36a39834ab4356eb92feeb7ad38e701edb51a
retro935 <edwinretro52@gmail.com>
2025-11-13 01:15:37 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot corriendo correctamente ✅"
+    return "Bot corriendo... ✅"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
