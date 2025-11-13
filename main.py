c77f8286c0824ee98057302844fdd9381cf98d32
retro935 <edwinretro52@gmail.com>
2025-11-13 03:05:35 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from keep_alive import keep_alive
-from telegram_bot import run_bot
-
-if __name__ == "__main__":
-    # Inicia el servidor mínimo (mantiene el contenedor vivo si usas Render)
-    keep_alive()
-    # Inicia el bot (blocking)
-    run_bot()
